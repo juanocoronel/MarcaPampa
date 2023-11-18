bc85957b8d64b7faf63cd705b923fc9bf1068d32
juanocoronel <juanocoronell@gmail.com>
2023-11-18 17:18:58 -0300

    Instalado Tailwind

--- front-end/src/routes/pages/HomeScreen.jsx
+++ front-end/src/routes/pages/HomeScreen.jsx
@@ -13,6 +13,9 @@ export const HomeScreen = () =>  {
         <h4>tecnologia: {usuario.tecnologia}</h4>
         <h4>email: {usuario.email}</h4>
         <h4>redes: {usuario.redes}</h4> 
+        <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+        Badge
+      </span>
       </>
     )
   }
