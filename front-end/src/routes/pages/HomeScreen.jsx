167e18905944e603aa1792ddb94b016811cea897
juanocoronel <juanocoronell@gmail.com>
2023-11-21 10:00:33 -0300

    Se comenzó el Home

--- front-end/src/routes/pages/HomeScreen.jsx
+++ front-end/src/routes/pages/HomeScreen.jsx
@@ -8,11 +8,40 @@ export const HomeScreen = () =>  {
 
     return (
       <>
-        <h1>Soy home </h1>
-        <h4>Nombre: {usuario.nombre}</h4>        
-        <h4>tecnologia: {usuario.tecnologia}</h4>
-        <h4>email: {usuario.email}</h4>
-        <h4>redes: {usuario.redes}</h4> 
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56" >
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+             Entra a nuestra Tienda{' '}
+              <a href="#" className="font-semibold text-green-700">
+                <span className="absolute inset-0" aria-hidden="true" />
+                Marca Pampa <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+Marca Pampa            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+            Donde el viento de la Pampa susurra historias, plasmamos momentos, y conectamos el alma gaucha con la esencia digital.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          </div>
       </>
     )
   }
